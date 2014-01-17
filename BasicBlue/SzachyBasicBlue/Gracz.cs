9b0be075b0d26479a6202987888d282cfa5f5c07
Paweł Rubinkowski <p.rubinkowski@hotmail.com>
2014-01-17 21:22:53 +0100

    Revert "Modyfikacja Estetyczna"
    This reverts commit 27658278d8efac59a4f120881806091dda5c48e3.

---
+++ BasicBlue/SzachyBasicBlue/Gracz.cs
@@ -0,0 +1,13 @@
+using System;
+using BasicBlue.SzachyBasicBlue;
+namespace SzachyBasicBlue {
+	public class Gracz : Silnik_Gracza  {
+		public Enums.Kolor_Gracza kolor;
+		private String silnikGracza;
+		public String zrobRuch;
+
+		private Gra gra;
+
+	}
+
+}
